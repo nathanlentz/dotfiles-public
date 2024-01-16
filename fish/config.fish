c3c4cf937a95f0ca61437d10c8b8ddde7f43efce
Nate Lentz <nlentz@ostusa.com>
2024-01-16 09:02:23 -0500

    neovim config

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,9 @@ alias lla "ll -A"
 alias g git
 command -qv nvim && alias vim nvim
 
+# dotnet3 
+alias dotnet3 /usr/local/share/dotnet/x64/dotnet
+
 set -gx EDITOR nvim
 
 set -gx PATH bin $PATH
@@ -29,28 +32,33 @@ set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH node_modules/.bin $PATH
 
 switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    source (dirname (status --current-filename))/config-linux.fish
-  case '*'
-    source (dirname (status --current-filename))/config-windows.fish
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
 end
+
+
+# Volta
+set -g VOLTA_HOME $HOME/.volta
+set -gx PATH $VOLTA_HOME/bin $PATH
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
-  source $LOCAL_CONFIG
+    source $LOCAL_CONFIG
 end
 
 
 if status is-interactive
-# Commands to run in interactive sessions can go here
-and not set -q TMUX
+    # Commands to run in interactive sessions can go here
+    and not set -q TMUX
     exec tmux
 end
 
 # pnpm
-set -gx PNPM_HOME "/Users/nlentz/Library/pnpm"
+set -gx PNPM_HOME /Users/nlentz/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
 [ -f ~/.inshellisense/key-bindings.fish ] && source ~/.inshellisense/key-bindings.fish
