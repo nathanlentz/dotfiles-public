3b8de0a731474c484953f3a9f1f6c41d0df0f1ac
Nate Lentz <nlentz@ostusa.com>
2024-03-09 08:08:09 -0500

    remove symbols-outline

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "dinhhuy258/git.nvim",
+    event = "BufReadPre",
+    opts = {
+      keymaps = {
+        -- Open blame window
+        blame = "<Leader>gb",
+        -- Open file/folder in git repository
+        browse = "<Leader>go",
+        quit_blame = "<Leader>gq",
+        diff = "<Leader>gd",
+        diff_close = "<Leader>gD",
+      },
+    },
+  },
+}
